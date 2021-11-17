3b90963305d60a77672af1d8e8e8a83d934e15dd
Nikolay Ryabykh <nryabykh@isgneuro.com>
2021-11-17 14:23:19 +0300

    light refactor in scala way

--- src/main/com/isgneuro/otp/fs/commands/Put.scala
+++ src/main/com/isgneuro/otp/fs/commands/Put.scala
@@ -1,7 +1,9 @@
 package com.isgneuro.otp.fs.commands
 
-import org.apache.spark.sql.{DataFrame, SaveMode}
+import org.apache.spark.sql.{DataFrame, DataFrameWriter, Row, SaveMode}
 import com.isgneuro.otp.fs.internals.Storage
+import org.apache.spark.sql.types.NullType
+import org.apache.spark.sql.functions.col
 import ot.dispatcher.sdk.core.SimpleQuery
 import ot.dispatcher.sdk.PluginUtils
 
@@ -17,14 +19,34 @@ class Put(sq: SimpleQuery, utils: PluginUtils) extends Storage(sq, utils) {
 
   private val partitionBy = getKeyword("partitionBy").map(_.split(",").map(_.trim))
 
+  private def castNullColsToString(df: DataFrame): DataFrame = {
+    val schema = df.schema
+    val colsCasted = df.columns.map{
+      colName => if (schema(colName).dataType == NullType) col(colName).cast("String") else col(colName)
+    }
+    df.select(colsCasted: _*)
+  }
+
+  private def createDfWriter: DataFrame => DataFrameWriter[Row] =
+    df => df.write.format(format).mode(mode).option("header", "true")
+
+  private def isAllColsExists: (Seq[String], DataFrame) => Boolean =
+    (partitions, df) => partitions.forall(df.columns.contains)
+
+  private def getMissedCols: (Seq[String], DataFrame) => Seq[String] =
+    (partitions, df) => partitions.filterNot(df.columns.contains)
+
   override def transform(_df: DataFrame): DataFrame = {
-    val dfw = _df.write.format(format).mode(mode).option("header", "true")
+    val dfw = (castNullColsToString andThen createDfWriter)(_df)
     partitionBy match {
-      case Some(partitions) if partitions.forall(_df.columns.contains) => dfw.partitionBy(partitions: _*).save(absolutePath)
+      case Some(partitions) if isAllColsExists(partitions, _df) =>
+        dfw.partitionBy(partitions: _*).save(absolutePath)
+
       case Some(partitions) => {
-        val missedCols = partitions.filterNot(_df.columns.contains)
+        val missedCols = getMissedCols(partitions, _df)
         sendError(s"Missed columns: '${missedCols.mkString(", ")}")
       }
+
       case _ => dfw.save(absolutePath)
     }
     _df
