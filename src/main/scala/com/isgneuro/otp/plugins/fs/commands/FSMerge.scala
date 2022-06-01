481a51f9d9bfd9f33f8c2a0a337432c0e388219d
DNikolaevKlg <dnikolaev@isgneuro.com>
2022-06-01 11:18:52 +0300

    Partially developed fsmerge command.

---
+++ src/main/scala/com/isgneuro/otp/plugins/fs/commands/FSMerge.scala
@@ -0,0 +1,66 @@
+package com.isgneuro.otp.plugins.fs.commands
+
+import com.isgneuro.otp.plugins.fs.config.BranchConfig
+import com.isgneuro.otp.plugins.fs.internals.Storage
+import com.isgneuro.otp.spark.OTLSparkSession
+import org.apache.spark.sql.{DataFrame, DataFrameWriter, Row, SaveMode}
+import ot.dispatcher.sdk.PluginUtils
+import ot.dispatcher.sdk.core.SimpleQuery
+import org.apache.spark.sql.functions._
+
+import java.io.File
+
+class FSMerge(sq: SimpleQuery, utils: PluginUtils) extends Storage(sq, utils) with OTLSparkSession {
+  private val isInferSchema: String = getKeyword("inferSchema").getOrElse("true")
+
+  private def createDfWriter: DataFrame => DataFrameWriter[Row] =
+    df => df.write.format(format).mode(SaveMode.Append).option("header", "true")
+
+  override def transform(_df: DataFrame): DataFrame = {
+    val modelPath = getmodelPath
+    val modelExists = new File(modelPath).exists
+    if(!modelExists)
+      sendError("Model " + model + " doesn't exists.")
+    var outBranch = "main"
+    val outBranchText = getKeyword("outbranch").getOrElse("main")
+    if (outBranchText != "main") {
+      val outBranchExists = new File(modelPath + "/" + outBranchText).exists
+      outBranch = if (outBranchExists) {
+        outBranchText
+      } else {
+        sendError("Branch " + outBranchText + " doesn't exists in model " + model + ". Use command fsbranch for new branch creation")
+      }
+    }
+    var inBranch = "main"
+    val inBranchText = getKeyword("inbranch").getOrElse("main")
+    if (inBranchText != "main") {
+      val inBranchExists = new File(modelPath + "/" + inBranchText).exists
+      inBranch = if (inBranchExists) {
+        inBranchText
+      } else {
+        sendError("Branch " + inBranchText + " doesn't exists in model " + model + ". Use command fsbranch for new branch creation")
+      }
+    }
+    if (outBranch == inBranch) {
+      sendError("Out and in branches are equal. Execution of operation is unreachable.")
+    }
+    val commonReader = spark.read
+      .format(format)
+      .option("header", "true")
+    val dfReader = if (format == "csv") commonReader.option("inferSchema", isInferSchema) else commonReader
+    val outBranchConfig = new BranchConfig
+    val outLastVersion = outBranchConfig.getLastVersion(modelPath, outBranch)
+    val outVersion = getKeyword("outbranchversion").getOrElse(outLastVersion)
+    val outDataPath = modelPath + "/" + outBranch + "/" + outVersion
+    val mergingData: DataFrame = _df
+      //dfReader.load(outDataPath)
+    val inBranchConfig = new BranchConfig
+    val inLastVersion = inBranchConfig.getLastVersion(modelPath, inBranch)
+    val inVersion = getKeyword("inbranchversion").getOrElse(inLastVersion)
+    val inDataPath =  modelPath + "/" + inBranch + "/" + inVersion
+    val dfWriter = createDfWriter(mergingData)
+    dfWriter.save(inDataPath)
+    mergingData.withColumn("outbranch", col(outBranch)).withColumn("outbranchversion", col(outVersion))
+      .withColumn("inbranch", col(inBranch)).withColumn("inbranchversion", col(inVersion))
+  }
+}
